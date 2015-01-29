035985348bfdcdf7ff5820a6939856a1d5999fc0
etresoft <jdaniel@etresoft.com>
2015-01-29 07:06:15 -0500

    Put empty back in memory list - but just one this time.

--- EtreCheck/HardwareCollector.m
+++ EtreCheck/HardwareCollector.m
@@ -517,7 +517,7 @@
       
     if([size isEqualToString: @"empty"])
       {
-      size = @"";
+      size = NSLocalizedString(@"Empty", NULL);
       type = @"";
       speed = @"";
       status = @"";
